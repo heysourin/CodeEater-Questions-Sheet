cdb2d2e33cf535c644e49b6540e957c4c7619d68
Sourin <98945276+heysourin@users.noreply.github.com>
2023-02-28 23:56:34 +0530

    L117. Time lock smart contract

---
+++ L117-TimeLock-Contract.sol
@@ -0,0 +1,181 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
+
+contract TimeLock {
+    error NotOwnerError();
+    error AlreadyQueuedError(bytes32 txId);
+    error TimestampNotInRangeError(uint256 blockTimestamp, uint256 timestamp);
+    error NotQueuedError(bytes32 txId);
+    error TimestampNotPassedError(uint256 blockTimestmap, uint256 timestamp);
+    error TimestampExpiredError(uint256 blockTimestamp, uint256 expiresAt);
+    error TxFailedError();
+
+    event Queue(
+        bytes32 indexed txId,
+        address indexed target,
+        uint256 value,
+        string func,
+        bytes data,
+        uint256 timestamp
+    );
+    event Execute(
+        bytes32 indexed txId,
+        address indexed target,
+        uint256 value,
+        string func,
+        bytes data,
+        uint256 timestamp
+    );
+    event Cancel(bytes32 indexed txId);
+
+    uint256 public constant MIN_DELAY = 10; // seconds
+    uint256 public constant MAX_DELAY = 1000; // seconds
+    uint256 public constant GRACE_PERIOD = 1000; // seconds
+
+    address public owner;
+    // tx id => queued
+    mapping(bytes32 => bool) public queued;
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    modifier onlyOwner() {
+        if (msg.sender != owner) {
+            revert NotOwnerError();
+        }
+        _;
+    }
+
+    receive() external payable {}
+
+    function getTxId(
+        address _target,
+        uint256 _value,
+        string calldata _func,
+        bytes calldata _data,
+        uint256 _timestamp
+    ) public pure returns (bytes32) {
+        return keccak256(abi.encode(_target, _value, _func, _data, _timestamp));
+    }
+
+    /**
+     * @param _target Address of contract or account to call
+     * @param _value Amount of ETH to send
+     * @param _func Function signature, for example "foo(address,uint256)"
+     * @param _data ABI encoded data send.
+     * @param _timestamp Timestamp after which the transaction can be executed.
+     */
+    function queue(
+        address _target,
+        uint256 _value,
+        string calldata _func,
+        bytes calldata _data,
+        uint256 _timestamp
+    ) external onlyOwner returns (bytes32 txId) {
+        txId = getTxId(_target, _value, _func, _data, _timestamp);
+        if (queued[txId]) {
+            revert AlreadyQueuedError(txId);
+        }
+        // ---|------------|---------------|-------
+        //  block    block + min     block + max
+        if (
+            _timestamp < block.timestamp + MIN_DELAY ||
+            _timestamp > block.timestamp + MAX_DELAY
+        ) {
+            revert TimestampNotInRangeError(block.timestamp, _timestamp);
+        }
+
+        queued[txId] = true;
+
+        emit Queue(txId, _target, _value, _func, _data, _timestamp);
+    }
+
+    function execute(
+        address _target,
+        uint256 _value,
+        string calldata _func,
+        bytes calldata _data,
+        uint256 _timestamp
+    ) external payable onlyOwner returns (bytes memory) {
+        bytes32 txId = getTxId(_target, _value, _func, _data, _timestamp);
+        if (!queued[txId]) {
+            revert NotQueuedError(txId);
+        }
+        // ----|-------------------|-------
+        //  timestamp    timestamp + grace period
+        if (block.timestamp < _timestamp) {
+            revert TimestampNotPassedError(block.timestamp, _timestamp);
+        }
+        if (block.timestamp > _timestamp + GRACE_PERIOD) {
+            revert TimestampExpiredError(
+                block.timestamp,
+                _timestamp + GRACE_PERIOD
+            );
+        }
+
+        queued[txId] = false;
+
+        // prepare data
+        bytes memory data;
+        if (bytes(_func).length > 0) {
+            // data = func selector + _data
+            data = abi.encodePacked(bytes4(keccak256(bytes(_func))), _data);
+        } else {
+            // call fallback with data
+            data = _data;
+        }
+
+        // call target
+        (bool ok, bytes memory res) = _target.call{value: _value}(data);
+        if (!ok) {
+            revert TxFailedError();
+        }
+
+        emit Execute(txId, _target, _value, _func, _data, _timestamp);
+
+        return res;
+    }
+
+    function cancel(bytes32 _txId) external onlyOwner {
+        if (!queued[_txId]) {
+            revert NotQueuedError(_txId);
+        }
+
+        queued[_txId] = false;
+
+        emit Cancel(_txId);
+    }
+}
+
+/*
+    The 'getTxId()' function takes in the target address, value, function signature, data and timestamp,
+and returns a unique ID for the transaction based on its inputs.
+
+    The 'queue()' function allows the owner to queue a transaction for future execution.
+The function takes in the target address, value, function signature, data and timestamp.
+It first checks if the transaction has already been queued by checking its ID against the queued mapping,
+and throws an error if it has. It then checks if the timestamp falls within the acceptable range, between
+the current block timestamp plus MIN_DELAY and MAX_DELAY. If it does not, it throws an error. Otherwise,
+it adds the transaction ID to the queued mapping and emits a Queue event.
+
+    The 'execute()' function allows the owner to execute a queued transaction. The function takes in the target address,
+value, function signature, data and timestamp. It first checks if the transaction has been queued by checking its
+ID against the queued mapping, and throws an error if it has not. It then checks if the current block timestamp is
+greater than or equal to the transaction's timestamp. If it is not, it throws an error. It also checks if the current
+block timestamp is less than or equal to the transaction's timestamp plus GRACE_PERIOD. If it is not, it throws an error.
+If both checks pass, it removes the transaction ID from the queued mapping, prepares the function call data,
+calls the target with the appropriate value and data, and emits an Execute event.
+*/
+
+contract TestTimeLock {
+    address public timeLock;
+
+    constructor(address _timeLock) {
+        timeLock = _timeLock;
+    }
+
+    function test() external view{
+        require(msg.sender == timeLock);
+    }
+}
